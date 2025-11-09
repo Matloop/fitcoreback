efd7e782793c7887420c509e9a29d6e034c4c2b7
Matheus Dias Estácio <150752803+Matloop@users.noreply.github.com>
2025-11-09 14:45:01 -0300

    Add files via upload

---
+++ src/main/java/com/matloop/fitcore/FitcoreApplication.java
@@ -0,0 +1,13 @@
+package com.matloop.fitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matloop/fitcore/FitcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matloop.fitcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
